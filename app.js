1f17a9c55c522dca605fe6fcd5e6240fb5a01a5f
Muhammad Bilal Raza <bilalraza2646@gmail.com>
2024-09-11 10:17:45 -0700

    Form.js

---
+++ app.js
@@ -0,0 +1,20 @@
+var emailArrays = [];
+var passwordArrays = [];
+function insertValue() {
+    var userEmail = document.getElementById("email").value;
+  
+    var userpassword = document.getElementById("password").value;
+  
+    emailArrays.push(userEmail);
+    passwordArrays.push(userpassword);
+
+    document.getElementById("data").innerHTML = `
+
+    <h3>Email : ${userEmail}</h3>
+    <br> <br>
+    <h3>password : ${userpassword}</h3>`
+
+// document.write("Your Email ;",emailArrays,"<br><br>","Your Password ;",userpassword )
+
+
+}
