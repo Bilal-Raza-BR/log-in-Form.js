ffa17c736f1f71916746d5d1fdea6cc4b1484341
Muhammad Bilal Raza <bilalraza2646@gmail.com>
2024-09-12 12:50:33 -0700

    Form2.js

--- app.js
+++ app.js
@@ -2,19 +2,56 @@ var emailArrays = [];
 var passwordArrays = [];
 function insertValue() {
     var userEmail = document.getElementById("email").value;
-  
+
     var userpassword = document.getElementById("password").value;
-  
+
     emailArrays.push(userEmail);
     passwordArrays.push(userpassword);
 
     document.getElementById("data").innerHTML = `
-
-    <h3 class="heading">Email : ${userEmail}</h3>
+<  <p class="heading"> <b>EMAIL</b> : ${emailArrays}</p>
     <br> <br>
-    <h3 class="heading">password : ${userpassword}</h3>`
+    <p class="heading"> <b>PASSWORD </b>: ${passwordArrays}</p>`
 
-// document.write("Your Email ;",emailArrays,"<br><br>","Your Password ;",userpassword )
+    // document.write("Your Email ;",emailArrays,"<br><br>","Your Password ;",userpassword )
+
+
+}
+
+function pop() {
+    var userEmail = document.getElementById("email").value;
+
+    var userpassword = document.getElementById("password").value;
+
+    emailArrays.pop();
+    passwordArrays.pop();
+
+    document.getElementById("data").innerHTML = `
+  <p class="heading"> <b>EMAIL</b> : ${emailArrays}</p>
+    <br> <br>
+    <p class="heading"> <b>PASSWORD </b>: ${passwordArrays}</p>`
+
+
+    // document.write("Your Email ;",emailArrays,"<br><br>","Your Password ;",userpassword )
+
+
+}
+
+function shift() {
+    var userEmail = document.getElementById("email").value;
+
+    var userpassword = document.getElementById("password").value;
+
+    emailArrays.shift();
+    passwordArrays.shift();
+
+    document.getElementById("data").innerHTML = `
+
+    <p class="heading"> <b>EMAIL</b> : ${emailArrays}</p>
+    <br> <br>
+    <p class="heading"> <b>PASSWORD </b>: ${passwordArrays}</p>`
+
+    // document.write("Your Email ;",emailArrays,"<br><br>","Your Password ;",userpassword )
 
 
 }
